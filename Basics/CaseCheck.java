4b039f3ec5bdd2b5f7de2f1afa06820b3f4e0a78
Richaingole <richaningole8704@gmail.com>
2026-08-22 16:44:23 +0530

    Add CaseCheck class to identify character case

---
+++ Basics/CaseCheck.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().trim().charAt(0);
+
+        if(ch>='a' && ch<='z'){
+            System.out.println("Lower Case!");
+        }else if(ch>='A' && ch<='Z'){
+            System.out.println("Upper Case!");
+        }else{
+            System.out.println("Not an alphabet!");
+        }
+        sc.close();
+    }
+    
+}
