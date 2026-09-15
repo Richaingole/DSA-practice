75e0cfff9fecce0d52cdc84cf553ba2a7d45c3f1
Richaingole <richaningole8704@gmail.com>
2026-09-15 14:10:59 +0530

    Print Armstrong number in a given range

---
+++ Basics/Armstrong.java
@@ -0,0 +1,26 @@
+public class Armstrong {
+    public static void main(String[] args){
+
+        for(int i=100; i<1000; i++){
+            if(isArmstrong(i)){
+                System.out.println(i+" ");
+            }
+        }
+        
+    }
+
+    static boolean isArmstrong(int n){
+        int original=n;
+        int sum=0;
+
+        while(n>0){
+
+            int a=n%10;
+            sum+=a*a*a;
+            n/=10;
+        }
+        return sum==original;
+
+    }
+    
+}
