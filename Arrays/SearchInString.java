d5d396ea5f1502e48b617535de52af80d10d5f07
Richaingole <richaningole8704@gmail.com>
2026-09-22 10:26:18 +0530

    Add linear search implementation in string
    Implements a boolean search method to searchma target char in a string using linear search, with edge case handling for empty strings.

---
+++ Arrays/SearchInString.java
@@ -0,0 +1,27 @@
+public class SearchInString {
+
+    public static void main(String[] args){
+
+        String name="Java Virtual Machine";
+        char target='r';
+        System.out.println(search(name, target));
+
+    }
+
+    static boolean search(String str, char target){
+
+        if(str.length()==0){
+            return false;
+        }
+
+        for(int i=0; i<str.length();  i++){
+            if(target==str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
+
+//output: true
